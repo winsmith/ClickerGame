55672af481e7734f86dac0ce0b8aae059dd8d098
Daniel Jilg <winsmith@winsmith.de>
2019-03-04 15:10:23 +0100

    Initial Commit

---
+++ Clicker Game Mac/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Clicker Game Mac
+//
+//  Created by Daniel Jilg on 04.03.19.
+//  Copyright © 2019 breakthesystem. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
